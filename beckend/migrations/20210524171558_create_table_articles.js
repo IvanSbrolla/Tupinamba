d027dd7ebfda57332f56c2b0202cee3323850594
Ivan Sbrolla <ivansbrolla1@gmail.com>
2021-05-24 14:29:55 -0300

    Migrations de criacao de tabelas categories e articles

---
+++ beckend/migrations/20210524171558_create_table_articles.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('articles', table => {
+    table.increments('id').primary()
+    table.string('name').notNull()
+    table.string('description', 1000).notNull()
+    table.string('imageUrl',1000)
+    table.binary('content').notNull()
+    table.integer('userId').unsigned().notNull()
+    table.foreign('userId').references('id').inTable('users')
+    table.integer('categoryId').unsigned().notNull()
+    table.foreign('categoryId').references('id').inTable('categories')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('articles')
+};
