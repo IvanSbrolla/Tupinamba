2e4f923a7115e5fb8cb6534c6b8b17231cce4c7a
Ivan Sbrolla <ivansbrolla1@gmail.com>
2021-05-29 14:02:37 -0300

    Correção de bug e segurança - CATEGORIES

--- backend/api/categories.js
+++ backend/api/categories.js
@@ -1,29 +1,36 @@
 module.exports = app => {
     const { isNumeric, isString, notIsEmptyOrNull, notExist } = app.api.validations
 
-    const save = (req, res) => {
+    const save = async (req, res) => {
         const category = { ...req.body }
-        if (req.query.id) category.id = req.query.id
 
         try {
-            if (category.id) isNumeric(category.id, 'O campo ID deve ser um numero')
+            if (req.query.id) {
+                isNumeric(req.query.id, 'O campo ID deve ser um numero')
+                const verifyIfHaveIdRegistered = await app.db('categories')
+                    .select('id')
+                    .where({id : req.query.id})
+                    .first()
+                    .catch( err => res.status(400).end({ "data": {}, "err": err }))
+                    notIsEmptyOrNull(verifyIfHaveIdRegistered,'Id de categoria nao encontrado para modificacao')
+            }
             notIsEmptyOrNull(category.name, 'O campo NAME nao pode ser do tipo null ou vazio')
             isString(category.name, 'O campo NAME deve ser um texto')
         } catch (err) {
             return res.status(400).json({ "data": {}, "err": err })
         }
 
-        if (category.id) {
+        if (req.query.id) {
             app.db('categories')
                 .update(category)
-                .where({ id: category.id })
-                .then(category => res.status(200).json({ "data": { category }, "err": false }))
+                .where({ id: req.query.id })
+                .then(res.status(200).send())
                 .catch(err => res.status(500).end({ "data": {}, "err": err }))
         }
         else {
             app.db('categories')
                 .insert(category)
-                .then(category => res.status(200).json({ "data": { category }, "err": false }))
+                .then(res.status(200).send())
                 .catch(err => res.status(500).end({ "data": {}, "err": err }))
         }
     }
@@ -53,70 +60,70 @@ module.exports = app => {
             .del()
             .then(category => notIsEmptyOrNull(category, 'Não foi possivel remover a categoria'))
             .then(res.status(204).end())
-            .catch (err => res.status(500).end({ "data": {}, "err": err }))
-}
-
-const withPath = categories => {
-    const getParent = (categories, parentId) => {
-        const parent = categories.filter(parent => parent.id === parentId)
-        return parent.length ? parent[0] : null
+            .catch(err => res.status(500).end({ "data": {}, "err": err }))
     }
 
-    const categoriesWithPath = categories.map(category => {
-        let path = category.name
-        let parent = getParent(categories, category.parentId)
-
-        while (parent) {
-            path = `${parent.name} > ${path}`
-            parent = getParent(categories, parent.parentId)
+    const withPath = categories => {
+        const getParent = (categories, parentId) => {
+            const parent = categories.filter(parent => parent.id === parentId)
+            return parent.length ? parent[0] : null
         }
 
-        return { ...category, path }
-    })
+        const categoriesWithPath = categories.map(category => {
+            let path = category.name
+            let parent = getParent(categories, category.parentId)
 
-    categoriesWithPath.sort((a, b) => {
-        if (a.path < b.path) return -1
-        if (a.path > b.path) return 1
-        return 0
-    })
+            while (parent) {
+                path = `${parent.name} > ${path}`
+                parent = getParent(categories, parent.parentId)
+            }
 
-    return categoriesWithPath
-}
+            return { ...category, path }
+        })
 
-const get = (req, res) => {
+        categoriesWithPath.sort((a, b) => {
+            if (a.path < b.path) return -1
+            if (a.path > b.path) return 1
+            return 0
+        })
 
-    if (req.query.id) {
+        return categoriesWithPath
+    }
+
+    const get = (req, res) => {
+
+        if (req.query.id) {
+            app.db('categories')
+                .select()
+                .where({ id: req.query.id })
+                .then(category => res.status(200).json({ "data": withPath(category), "err": false }))
+                .catch(err => res.status(500).end({ "data": {}, "err": err }))
+        }
+        else {
+            app.db('categories')
+                .select()
+                .then(categories => res.status(200).send({ "data": withPath(categories), "err": false }))
+                .catch(err => res.status(500).end({ "data": {}, "err": err }))
+        }
+    }
+
+    const toTree = (categories, tree) => {
+        if (!tree) tree = categories.filter(c => !c.parentId)
+        tree = tree.map(parentNode => {
+            const isChild = node => node.parentId == parentNode.id
+            parentNode.children = toTree(categories, categories.filter(isChild))
+            return parentNode
+        })
+        return tree
+    }
+
+
+    const getTree = (req, res) => {
         app.db('categories')
             .select()
-            .where({ id: req.query.id })
-            .then(category => res.status(200).json({ "data": withPath(category), "err": false }))
+            .then(categories => res.status(200).json(toTree(withPath(categories))))
             .catch(err => res.status(500).end({ "data": {}, "err": err }))
     }
-    else {
-        app.db('categories')
-            .select()
-            .then(categories => res.status(200).send({ "data": withPath(categories), "err": false }))
-            .catch(err => res.status(500).end({ "data": {}, "err": err }))
-    }
-}
 
-const toTree = (categories, tree) => {
-    if (!tree) tree = categories.filter(c => !c.parentId)
-    tree = tree.map(parentNode => {
-        const isChild = node => node.parentId == parentNode.id
-        parentNode.children = toTree(categories, categories.filter(isChild))
-        return parentNode
-    })
-    return tree
-}
-
-
-const getTree = (req, res) => {
-    app.db('categories')
-        .select()
-        .then(categories => res.status(200).json(toTree(withPath(categories))))
-        .catch(err => res.status(500).end({ "data": {}, "err": err }))
-}
-
-return { save, remove, get, getTree }
+    return { save, remove, get, getTree }
 }
